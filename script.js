b92275ff00867ec2325226dd61423c92c3596c3f
anaomart <37917804+anaomart@users.noreply.github.com>
2022-04-07 11:17:34 +0200

    Update script.js
    adding local storage

--- script.js
+++ script.js
@@ -14,8 +14,7 @@ window.onload = function() {
 }
 
 // Adding the task 
-AddButton.onclick = function() {
-
+AddButton.onclick = function Add() {
     //check if the input is empty 
     if (inPut.value === "") {
         Swal.fire("Can't be empty")
@@ -31,6 +30,8 @@ AddButton.onclick = function() {
         })
     } else {
         count += 1;
+        //Add to localStorage
+        localStorage.setItem(inPut.value, inPut.value);
         // remove the task massage 
         noTaskMsg.remove();
         // create span element 
@@ -38,7 +39,7 @@ AddButton.onclick = function() {
         // create delete btn 
         let deleteButton = document.createElement('span');
         //create  text of the span (new task);
-        let text = document.createTextNode(inPut.value);
+        let text = document.createTextNode(localStorage.getItem(inPut.value));
         // create  text of delete btn 
         let deleteText = document.createTextNode("Delete");
         // add text to the main span (add task)  and give it class
@@ -65,7 +66,7 @@ document.addEventListener('click', function(e) {
             let taskName = e.target.parentNode.firstChild;
             let indexOfTask = tasksArray.indexOf(taskName.textContent);
             tasksArray[indexOfTask] = '';
-            count -= 1;
+            localStorage.removeItem(taskName.textContent)
             if (taskContainer.childElementCount == 0) {
                 taskContainer.appendChild(noTaskMsg);
 
@@ -82,19 +83,43 @@ deleteAll.onclick = function() {
         taskContainer.innerHTML = "";
         taskContainer.appendChild(noTaskMsg);
         tasksArray = [];
+        localStorage.clear();
     }
     // all done btn ;
 allDone.onclick = function() {
     let allTasks = document.querySelectorAll('.task-box');
 
     allTasks.forEach(function(e) {
-        e.classList.add('finished');
+        e.classList.toggle('finished');
     });
 };
 // Number of tasks and completed tasks
 function numberOfTasks() {
-    tasksCount.textContent = count;
-
+    tasksCount.textContent = document.querySelectorAll('.task-box').length;
     taskCompleted.textContent = document.querySelectorAll('.finished').length;
 
+}
+
+window.onload = function() {
+    let keys = Object.keys(localStorage);
+    keys.forEach(function(key) {
+        noTaskMsg.remove();
+        let mainSpan = document.createElement('span');
+        // create delete btn 
+        let deleteButton = document.createElement('span');
+        //create  text of the span (new task);
+        let text = document.createTextNode(localStorage.getItem(key));
+        // create  text of delete btn 
+        let deleteText = document.createTextNode("Delete");
+        // add text to the main span (add task)  and give it class
+        mainSpan.appendChild(text);
+        mainSpan.className = 'task-box';
+        // add text to delete btn  and give it class 
+        deleteButton.appendChild(deleteText);
+        deleteButton.className = 'delete'
+            //add delete button to the main span 
+        mainSpan.appendChild(deleteButton)
+            // add the main span to page 
+        taskContainer.appendChild(mainSpan)
+    })
 }
